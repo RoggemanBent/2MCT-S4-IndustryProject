b8c22144b06158bd6f045dfb8383f2ad4f550d3c
Arthur d'Hooge <arthur.dhooge@outlook.com>
2021-06-09 15:47:41 +0200

    installed tensorboard for visuals and training monitoring

--- AI/supervisedNeuralNet/visualize.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.utils import plot_model
-
-model = tf.keras.models.load_model('AI\supervisedNeuralNet\model.h5')
-model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-
-plot_model(model, to_file='AI\supervisedNeuralNet\model_vis.png')
